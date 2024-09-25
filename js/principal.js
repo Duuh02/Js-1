97f1a276bee93540c419e331f23b7731325e21ba
Duuh02 <163295805+Duuh02@users.noreply.github.com>
2024-09-25 09:07:25 -0300

    123

--- js/principal.js
+++ js/principal.js
@@ -1,11 +1,11 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista"
+titulo.textContent = "Aparecida Nutricionista";
 
-var pacientes = document.querySelectorAll('paciente');
+var pacientes = document.querySelectorAll('.paciente');
 
-for(let i = 0; i < pacientes.length; i ++){
+for(let i = 0; i < pacientes.length;i ++){
     var peso = pacientes[i].querySelector('.info-peso').textContent;
-    var altura = pacientes[i].querySelector('info-altura').textContent;
-    imc = peso / (altura*altura);
-    pacientes[i].querySelector('.info-imc').textContent = imc;
+    var altura = pacientes[i].querySelector('.info-altura').textContent;
+    var imc = peso / (altura*altura);
+    pacientes[i].querySelector('info-imc').textContent = imc;
 }
